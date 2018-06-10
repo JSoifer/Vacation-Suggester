2257cc06a2dbca75606f45e3266b78cb18ca5f9a
Julianne Soifer <juliannesoifer@gmail.com>
2018-06-10 10:58:45 -0700

    add more javascript to give varied results based on survey results.

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,30 @@
+var score = function(season, purpose) {
+  return season + purpose;
+};
+
+
+
 $(document).ready(function() {
   $("form#survey").submit(function(event) {
+
     var name = $("input#name").val();
-    var season = parseInt($("select#season")).val();
-    var purpose = $("select#purpose").val();
+    var season = parseInt($("select#season").val());
+    var purpose = parseInt($("select#purpose").val());
     var independance = $("select#independance").val();
     var activity = $("select#activity").val();
 
-  if (season === 4) {
-    $("#Caribbean").show();
+  if (season === 4 && purpose === 1) {
+    $("#caribbean").show();
+  } else if (season === 4 && purpose === 2) {
+    $("#ecuador").show();
+  } else if (season === 100 && purpose === 2) {
+    $("#ireland").show();
+  } else if (score === 100 && purpose === 1) {
+    $("#ecuador").show();
+  } else {
+    $("#ireland").show();
   }
 
-
-    event.preventDefault();
+  event.preventDefault();
   });
 });
